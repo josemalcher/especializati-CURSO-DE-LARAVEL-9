24789772023e836fbc86c7da9e3dbe5b0542998c
Jose Malcher Jr <contato@josemalcher.net>
2022-12-02 09:29:08 -0300

    12 - Trabalhar com Templates Blade no Laravel

--- projaula1/resources/views/users/index.blade.php
+++ projaula1/resources/views/users/index.blade.php
@@ -1,8 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem dos Usuários')
+
+@section('content')
 <h1>Users</h1>
 <hr>
 
 <ul>
     @foreach($users as $user)
         <li>{{$user->name}} - {{$user->email}} | <a href="{{route('users.show', $user->id)}}">Detalhes</a></li>
+
     @endforeach
 </ul>
+@endsection
--- projaula1/resources/views/users/show.blade.php
+++ projaula1/resources/views/users/show.blade.php
@@ -1,5 +1,11 @@
+@extends('layouts.app')
+
+@section('title', "Dados do Usuário $user->name")
+
+@section('content')
 <h1>Dadoso do usuário {{$user->name}}</h1>
 <ul>
     <li>{{$user->name}}</li>
     <li>{{$user->email}}</li>
 </ul>
+@endsection
